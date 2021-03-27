0825756a3f28a6f28f825444c6c94563accfcc25
Mariana Vázquez Silva <67400256+MarianaNezuko@users.noreply.github.com>
2021-03-27 00:11:38 -0600

    Add files via upload
    Conversor que recibe números binarios y los devuelve en hexadecimal

---
+++ MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Mariana
+ * Fecha: 25/03/2021
+ * Hora: 12:03 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace Frame
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtBin;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtHexa;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtBin = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtHexa = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(22, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(138, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Número Binario:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// txtBin
+			// 
+			this.txtBin.Location = new System.Drawing.Point(22, 45);
+			this.txtBin.Name = "txtBin";
+			this.txtBin.Size = new System.Drawing.Size(198, 20);
+			this.txtBin.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(22, 88);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(198, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Número Hexadecimal:";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Pink;
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+			this.button1.Location = new System.Drawing.Point(259, 42);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Convertir";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// txtHexa
+			// 
+			this.txtHexa.Location = new System.Drawing.Point(22, 124);
+			this.txtHexa.Name = "txtHexa";
+			this.txtHexa.ReadOnly = true;
+			this.txtHexa.Size = new System.Drawing.Size(198, 20);
+			this.txtHexa.TabIndex = 4;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Thistle;
+			this.ClientSize = new System.Drawing.Size(421, 177);
+			this.Controls.Add(this.txtHexa);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtBin);
+			this.Controls.Add(this.label1);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Conversor";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Mariana
+ * Fecha: 25/03/2021
+ * Hora: 12:03 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Frame
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			convertir();
+	
+		}
+		
+		public  void convertir() {
+			
+			Console.WriteLine("Número Binario: ");
+			
+			int numero = int.Parse(txtBin.Text);
+			int count;
+			int acum;
+			int aux;
+			String resultado = "";
+			
+			
+			while (numero > 0) {
+				
+				count = 0;
+				acum = 0;
+				
+				while (count < 4){
+					 
+					aux = numero%10;
+					if (aux == 1) {
+						if (count == 0) {
+							acum = acum+1;
+						} else if (count == 1) {
+							acum = acum+2;
+						} else if (count == 2) {
+							acum = acum+4;
+						} else if (count == 3) {
+							acum = acum+8;
+						}
+					}
+					
+					numero = numero/10;
+					count++;
+				}
+				
+				if (acum <= 9) {
+					resultado = acum + resultado;
+				} else if (acum > 9){
+					if (acum == 10) {
+						resultado = "A" + resultado;
+					} else if (acum == 11){
+						resultado = "B" + resultado;
+					} else if (acum == 12){
+						resultado = "C" + resultado;
+					} else if (acum == 13){
+						resultado = "D" + resultado;
+					} else if (acum == 14){
+						resultado = "E" + resultado;
+					} else if (acum == 15){
+						resultado = "F" + resultado;
+					}
+				}
+				
+				txtHexa.Text = resultado.ToString();
+			}
+			
+		}
+	}
+}
